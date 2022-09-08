3340971bdb70cac0381cfb4c633a0c70f1d939bf
Lawj90 <lausonarthur@gmail.com>
2022-09-08 11:21:15 -0700

    Task 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,3 +2,10 @@
 * main - print the string in the put function
 *
 *Description: using the main function
+*Return: Always 0 (Successful)
+*/
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
