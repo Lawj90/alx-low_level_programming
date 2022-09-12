de3ba808282336052a3bbccf5862d7226d41b24c
Lawj90 <lausonarthur@gmail.com>
2022-09-12 11:11:12 -0700

    Task 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Prints the numbers 0 to 9
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
