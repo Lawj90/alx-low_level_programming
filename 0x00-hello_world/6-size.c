3aba6b915d588950db0412eb6f48380214083774
Lawj90 <lausonarthur@gmail.com>
2022-09-12 17:09:43 -0700

    Task 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - print the size of many var types
+ *
  * Description: using the main function
  * this program prints "This program prints the size of various types"
- * return: 1
+ * Return: 0
  */
 int main(void)
 {
@@ -12,12 +13,13 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %i byte(s)\n", sizeof(c));
 	printf("Size of an int: %i byte(s)\n", sizeof(i));
 	printf("Size of a long int: %i byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return (1);
+	return (0);
 
 }
 
